2f848ce3537a84f9e8ef06c3d24dedf99d9060a3
rohitnair <rohit.nair@ucalgary.ca>
2023-02-10 23:45:46 -0700

    Updated skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      animation:{
+        fade: 'fadeOut 5s ease-in-out',
+      },
       fontFamily:{
         Roboto: "Roboto",
         Lobster: "Lobster",
